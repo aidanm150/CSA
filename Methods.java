87db58691ba86427305f69e594beab7a7b990e94
aidanm150 <aidanmerritt@billericak12.com>
2023-09-14 13:36:16 -0400

    Methods

---
+++ Methods.java
@@ -0,0 +1,28 @@
+public class Methods {
+    public static void main(String [] args){
+        method1(x:5);
+        method(x:92);
+        int r1 = method2(name:"Arjun", x:7);
+        System.out.println(r1);
+        int r2 = method2(name:"Serena", x:8);
+        System.out.println(r2);
+        System.out.println(method3(4,5,5));
+        // int r3 = method3(4, 5.5)
+        // System.out.println(r3);
+
+    }
+    public static double method3(int y double z){
+        double answer = y+6*z-2;
+        return answer;
+    }
+    public static int method2(String name int x){
+        System.out.println("Hello " + name)
+        int rem = x%2;
+        return rem;
+    }
+    public static void method1(int x){
+        int y = x + 1;
+        System.out.println("One more than " + y);
+
+    }
+}
