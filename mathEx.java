14f0e670150b94d43fe859614d77c72e2d0499e5
aidanm150 <aidanmerritt@billericak12.com>
2023-09-20 13:49:08 -0400

    mathEx

--- mathEx.java
+++ mathEx.java
@@ -3,12 +3,16 @@ public class mathEx {
         int swapped1 = swap(21);
         System.out.println(swapped1);
         
+        int swapped2 = swap(123);
+        System.out.println(swapped2);
     }
 
     public static int swap(int x){
-        int y = (x%10)*10;
-        int m = (x/10);
-        int k = y + m;
+        int r = (x%100);
+        int q = x - r;
+        int y = (r%10)*10;
+        int m = (r/10);
+        int k = q + y + m;
         return k;
     }
 }
