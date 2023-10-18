ac710be26b99edb0f624d83fc81a984d794909df
aidanm150 <aidanmerritt@billericak12.com>
2023-10-18 14:00:31 -0400

    Circle

---
+++ Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+    private double radius;
+    
+    public Circle(double r){
+        r = radius;
+        double area = Math.PI * Math.pow(r,2.0);
+        double circumference = 2 * Math.PI * r;
+    }
+
+    public double area(){
+        
+    }
+}
