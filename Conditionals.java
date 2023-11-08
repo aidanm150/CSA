3c7f6b5c96479c4a3a82f412296382cf2cfcd181
aidanm150 <aidanmerritt@billericak12.com>
2023-11-08 15:55:34 -0500

    conditionals

--- Conditionals.java
+++ Conditionals.java
@@ -74,7 +74,7 @@ public class Conditionals {
     public int blackjack(int x, int y){
         if(x<=21||y<=21){
             if(y<=21&&x<=21){
-                if(x<y){
+                if(x<=y){
                     return y;
                 }
                 else{
@@ -82,44 +82,53 @@ public class Conditionals {
                 }
             }
             else{
-                if(y<x && x<=21){
+                if(y>x && x<=21){
                     return x;
                 }
-                else if(y>x&&y<=21){}
+                else if(y<x && y<=21){}
                     return y;
                 }
             }
         else{
             return 0;
         }
-        }
-    //     else if(y<=21 && x<y){
-    //         return y;
-    //     }
-    //     else{
-    //         return 0;
-    //     }
-    // }
+    }
 
     public static void main(String [] args){
         Conditionals x = new Conditionals();
         System.out.println("chimpTrouble method");
         System.out.println(x.chimpTrouble(true,true));
+        System.out.println(x.chimpTrouble(false,false));
+        System.out.println(x.chimpTrouble(true,false));
         System.out.println(x.chimpTrouble(false,true));
         System.out.println("negPos method");
-        System.out.println(x.negPos(-2,3,true));
-        System.out.println(x.negPos(-3,-2,true));
-        System.out.println(x.negPos(-3,-2,false));
-        System.out.println(x.negPos(3,-2,false));
+        System.out.println(x.negPos(1,-1,false));
+        System.out.println(x.negPos(-1,1,false));
+        System.out.println(x.negPos(1,1,false));
+        System.out.println(x.negPos(-1,-1,false));
+        System.out.println(x.negPos(4,-5,true));
+        System.out.println(x.negPos(-4,5,true));
+        System.out.println(x.negPos(4,5,true));
         System.out.println("pickUpPhone Method");
-        System.out.println(x.pickUpPhone(false, false, false));
-        System.out.println(x.pickUpPhone(true, true, false));
-        System.out.println(x.pickUpPhone(true,false,false));
+        System.out.println(x.pickUpPhone(false, false, true));
         System.out.println(x.pickUpPhone(true, true, true));
+        System.out.println(x.pickUpPhone(true,true,false));
+        System.out.println(x.pickUpPhone(false, true, false));
+        System.out.println(x.pickUpPhone(true, false, false));
+        System.out.println(x.pickUpPhone(false, false, false));
+        System.out.println("setAlarm Method");
+        System.out.println(x.setAlarm(1, false));
+        System.out.println(x.setAlarm(5, false));
+        System.out.println(x.setAlarm(0, false));
+        System.out.println(x.setAlarm(2, true));
+        System.out.println(x.setAlarm(4, true));
+        System.out.println(x.setAlarm(6, true));
         System.out.println("onesDigitSame Method");
-        System.out.println(x.onesDigitSame(12, 22, 32));
-        System.out.println(x.onesDigitSame(11, 13, 12));
-        System.out.println(x.onesDigitSame(2, 2, 4));
+        System.out.println(x.onesDigitSame(23, 19, 13));
+        System.out.println(x.onesDigitSame(23, 19, 12));
+        System.out.println(x.onesDigitSame(23, 19, 3));
+        System.out.println(x.onesDigitSame(423, 13, 3));
+        System.out.println(x.onesDigitSame(23, 29, 25));
         System.out.println("Blackjack Method");
         System.out.println(x.blackjack(19,21));
         System.out.println(x.blackjack(21,19));
