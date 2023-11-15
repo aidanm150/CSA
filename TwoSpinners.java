1fa7d6339fe1ea7842902544c94e6317ad5960ff
aidanm150 <aidanmerritt@billericak12.com>
2023-11-15 13:58:57 -0500

    hi:3

--- TwoSpinners.java
+++ TwoSpinners.java
@@ -1,4 +1,5 @@
 public class TwoSpinners{
+
  	public int spin(int min, int max){
      	int result=0;
         result=(int)((Math.random()*max)+min);
@@ -8,13 +9,32 @@ public class TwoSpinners{
  	/** Simulates one round of the game as described in part (b).
   	*/
  	public void playRound(){
-        public int pPoints = 0;
+		int points=0;
         int player=spin(1,10);
         int computer=spin(2,8);
         if(player<computer){
-            pPoints+=computer-player;
-            System.out.println("You Lose.");
+            points-=computer-player;
+            System.out.println("You Lose. "+points);
         }
+		else if(player>computer){
+			points+=player-computer;
+			System.out.println("You win! "+points);
+		}
+		else{
+			player+=spin(1,10);
+			computer+=spin(2,8);
+			if(player>computer){
+				points+=player-computer;
+				System.out.println("You win! "+points);
+			}
+			else if(player<computer){
+				points-=computer-player;
+				System.out.println("You lose. "+points);
+			}
+			else{
+				System.out.println("Tie, no points for anyone.");
+			}
+		}
 	}
 
  	public static void main(String[] args) {
