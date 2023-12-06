44673eca0a44cf6994ba1521eca20b31cf38dd3b
aidanm150 <aidanmerritt@billericak12.com>
2023-12-06 17:41:13 -0500

    hai

--- Chapter 3/WhileLoops.java
+++ Chapter 3/WhileLoops.java
@@ -1,6 +1,6 @@
 public class WhileLoops{
 
-    public int addOdds(int n){
+    public static int addOdds(int n){
         int sum=0;
         while(n>1){
             if(n%2==0){
@@ -14,8 +14,18 @@ public class WhileLoops{
         return sum;
     }
 
+    public int sumDigits(int x){
+        int i = 10;
+        int sum=0;
+        while(x!=0){
+            x=x%i;
+            i = i*10;
+            sum=sum+x;
+        }
+        return sum;
+    }
     public static void main(String[] args){
-        WhileLoops ab = new WhileLoops();
-        System.out.println(ab.addOdds(8));
+        System.out.println(addOdds(8));
+        System.out.println(10/100);
     }
 }
