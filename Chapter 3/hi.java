a6324e7f5f8a967bf9e4b6a01d34f7e4e8a506fe
aidanm150 <aidanmerritt@billericak12.com>
2023-12-22 23:59:16 -0500

    hai

---
+++ Chapter 3/hi.java
@@ -0,0 +1,14 @@
+public class hi {
+    private boolean IsEven(int presents){
+        if(presents == 1||presents==3||presents==5||presents==7||presents==9||presents==11){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String args[]){
+        hi ab = new hi();
+        System.out.println(ab.IsEven(2));
+    }
+}
