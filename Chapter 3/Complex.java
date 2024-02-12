1bd665f6a749e2833e4e7c085d8cdee973a82a55
aidanm150 <aidanmerritt@billericak12.com>
2024-02-12 13:59:30 -0500

    heyyy

--- Chapter 3/Complex.java
+++ Chapter 3/Complex.java
@@ -27,16 +27,39 @@ public class Complex{
         return new Complex((x+other.x),(y+other.y));
     }
 
+    public Complex add(double a, double b, double c, double d){
+        return new Complex((a+c),(b+d));
+    }
+
     public Complex multiply(Complex other){
         Complex p = new Complex((x*other.x)-((y*other.y)*1),(x*other.y)+(y*other.x));
         return p;
     }
 
+    public Complex multiply(double a, double b, double c, double d){
+        Complex p = new Complex((a*c)-((b*d)*1),(a*d)+(b*c));
+        return p;
+
+    }
+
     public static void main(String args[]){
         Complex a = new Complex(2.0,3.0);
         Complex b = new Complex(3.0,1.0);
+        Complex c = new Complex(3.0,-2.0);
+        Complex d = new Complex(-5.0,2.0);
+        Complex e = new Complex(4.5);
+        Complex f = new Complex(0,5);
         System.out.println(a.abs());
+        System.out.println(b.abs());
+        System.out.println(c.abs());
+        System.out.println(d.abs());
+        System.out.println(e.abs());
+        System.out.println(f.abs());
         System.out.println(a.add(b));
+        System.out.println(b.add(c));
+        System.out.println(e.add(f));
         System.out.println(a.multiply(b));
+        System.out.println(c.multiply(d));
+        System.out.println(e.multiply(f));
     }
 }   
---
+++ Chapter 3/SoccerTeam.java
@@ -0,0 +1,26 @@
+public class SoccerTeam {
+    private int wins;
+    private int losses;
+    private int ties;
+
+    public SoccerTeam(){
+        wins = 0;
+        losses = 0;
+        ties = 0;
+    }
+
+    public void played(SoccerTeam other, int myScore, int otherScore){
+        if(myScore>otherScore){
+            this.wins++;
+            other.losses++;
+        }
+        else if(otherScore>myScore){
+            other.wins++;
+            this.losses++;
+        }
+        else if(otherScore==myScore){
+            this.ties++;
+            other.ties++;
+        }
+    }
+}
